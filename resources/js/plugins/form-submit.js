b3d837fb1e2433d95cd2fb24d4abb84ae8f332f1
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-08 19:25:35 -0400

    Modo carregamento

--- resources/js/plugins/form-submit.js
+++ resources/js/plugins/form-submit.js
@@ -7,6 +7,7 @@
         let form = $(this);
         let data = new FormData(form[0]);
         let action = form.attr("action");
+        let submitter = $(e.originalEvent.submitter);
 
         messageArea = form.find(".message-area").length ? form.find(".message-area") : messageArea;
 
@@ -17,6 +18,11 @@
             dataType: "json",
             contentType: false,
             processData: false,
+
+            beforeSend: function () {
+                addLoadingMode(submitter);
+                addBackdrop("formSubmitBkdp", "absolute");
+            },
 
             success: function (response) {
                 if (response.reload) {
@@ -34,10 +40,83 @@
                 }
 
                 addFormErrors(form, response.errors);
+            },
+
+            complete: function () {
+                removeLoadingMode(submitter);
+                removeBackdrop("formSubmitBkdp");
             }
         });
     };
 
+
+    /**
+     *
+     * FUNÇÕES: BACKDROP
+     *
+     */
+
+    /**
+     * @param {String} id id para o backdrop
+     * @param {String} position tipo de posicionamento. Padrão é 'absolute'
+     * @param {jQuery} container onde inserir o backdrop.
+     * @param {String} effect efeito do jquery-ui. Padrão é 'fade'
+     */
+    function addBackdrop(id, position, container, effect) {
+        let cntnr = container ?? $("body");
+        let efct = effect ?? "fade";
+        let bkdrop = $(`<div class="mback" id="${id}"></div>`).css({
+            "background-color": "rgb(0, 0, 0, 0.125)",
+            width: "100%",
+            height: "100%",
+            position: position ?? "absolute",
+            top: 0,
+            left: 0,
+            "z-index": 998,
+        }).hide();
+
+        cntnr.append(bkdrop.show(efct));
+    }
+
+    /**
+     * @param {String} id id do backdrop a ser removido
+     * @param {String} container local onde procurar o backdrop. Por padrão busca por todo o 'body'
+     * @param {String} effect efeito do jquery-ui. Padrão é 'fade'
+     */
+    function removeBackdrop(id, container, effect) {
+        let cntnr = $(container ?? "body");
+        let efct = effect ?? "fade";
+
+        cntnr.find("#" + id).hide(efct, function () {
+            $(this).remove();
+        });
+    }
+
+    /**
+     *
+     * FUNÇÕES: BOTÕES
+     *
+     */
+
+    /**
+     * @param {jQuery} buttonObject objeto jQuery do botão
+     */
+    function addLoadingMode(buttonObject) {
+        buttonObject
+            .removeClass(buttonObject.attr("data-active-icon"))
+            .addClass(buttonObject.attr("data-alt-icon"))
+            .prop("disabled", true);
+    }
+
+    /**
+     * @param {jQuery} buttonObject objeto jQuery do botão
+     */
+    function removeLoadingMode(buttonObject) {
+        buttonObject
+            .addClass(buttonObject.attr("data-active-icon"))
+            .removeClass(buttonObject.attr("data-alt-icon"))
+            .prop("disabled", false);
+    }
 
     /**
      * @param {jQuery} formObject
