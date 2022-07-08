0815e0bc5332e668d8ae649a74737b6d316b0a90
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-08 17:25:24 -0400

    Configurações, pacotes e traduções

---
+++ resources/lang/pt-br/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Sua senha foi atualizada!',
+    'sent' => 'Um email foi enviado com seu link de recuperação!',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'O token de recuperação de senha é inválido.',
+    'user' => "Não foi encontrado um usuário com este email.",
+
+];
