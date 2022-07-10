c1a5b0d7a891dd419c19ef8447807d175c0aa873
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-10 10:02:14 -0400

    Cards

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Admin Home</h1>
+    {{-- admim home cards --}}
+    <div class="row justify-content-center cards-list">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-6 col-xl-4 mb-4">
+            <div class="card card-body d-flex flex-row align-items-center cards-list-item">
+                <i class="icon bi bi-people-fill"></i>
+                <div class="card-item-content">
+                    <h5 class="h3 mb-0">Usuários</h5>
+                    <div>
+                        <small>
+                            <a href="{{ route('admin.users.index', ['filter' => true, 'status' => 'unverified']) }}">Não
+                                verificados</a>
+                            <span class="mx-1">|</span>
+                            <a
+                                href="{{ route('admin.users.index', ['filter' => true, 'status' => 'verified']) }}">Verificados</a>
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-6 col-xl-4 mb-4">
+            <div class="card card-body d-flex flex-row align-items-center cards-list-item">
+                <i class="icon bi bi-app-indicator"></i>
+                <div class="card-item-content">
+                    <h5 class="h3 mb-0">Example</h5>
+                    <div>
+                        <small>
+                            <a href="">Example #1</a>
+                            <span class="mx-1">|</span>
+                            <a href="">Example #2</a>
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
