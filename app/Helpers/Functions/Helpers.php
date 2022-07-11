7e0627b214c0965d1edca681bea7783de87f8f0c
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-11 08:37:58 -0400

    Ref

--- app/Helpers/Functions/Helpers.php
+++ app/Helpers/Functions/Helpers.php
@@ -1,28 +1,29 @@
 <?php
 
 /**
+ * Obtém valor de $data
  * @param [type] $data
  * @param [type] $key
  * @return null|string
  */
 function input_value($data, $key)
 {
-    if (!$data) {
-        return null;
-    }
+    if (!$data) return null;
+
     $data = is_array($data) ? (object) $data : $data;
+
     return $data->$key ?? null;
 }
 
-
 /**
  *
+ * Funções helpers para o modelo User
  * MODELS
- *
  *
  */
 
 /**
+ * Obtém array de gêneros do modelo User
  * @return array
  */
 function m_user_genders(): array
@@ -31,6 +32,7 @@ function m_user_genders(): array
 }
 
 /**
+ * Obtém array de níveis de usuário do modelo User
  * @return array
  */
 function m_user_levels(): array
