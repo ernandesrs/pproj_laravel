6c3e099cc614af83fbaf7d4021b1fc4c4462630d
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-11 20:06:33 -0400

    Model e migrations para Page

---
+++ database/migrations/2022_07_11_192516_create_pages_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("title")->fulltext()->nullable(false);
+            $table->string("description")->fulltext()->nullable(false);
+            $table->string("cover")->nullable();
+            $table->string("lang", 5)->nullable(false)->default(config("app.locale"));
+            $table->string("content_type")->nullable(false)->default("text");
+            $table->string("content")->nullable(true);
+
+            $table->string("status")->nullable(false)->default("draft");
+            $table->timestamp("published_at")->nullable(true);
+            $table->timestamp("scheduled_to")->nullable(true);
+
+            // FOREIGN KEYS
+            $table->foreignId("author")->unsigned()->nullable(true);
+            $table->foreignId("slug")->unsigned()->nullable(false);
+
+            $table->foreign("author")->references("id")->on("users")->nullOnDelete();
+            $table->foreign("slug")->references("id")->on("slugs")->restrictOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
