e91792704b7a3d374899c392c75e54a21c7635fe
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-07-13 09:50:46 -0400

    Painel Administrativo: páginas

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -23,6 +23,27 @@
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-6 col-xl-4 mb-4">
             <div class="card card-body d-flex flex-row align-items-center cards-list-item">
+                {{ icon_elem('pageEarmarkText') }}
+                <div class="card-item-content">
+                    <h5 class="h3 mb-0">Páginas</h5>
+                    <div>
+                        <small>
+                            <a
+                                href="{{ route('admin.pages.index', ['filter' => true, 'status' => 'published']) }}">Publicadas</a>
+                            <span class="mx-1">|</span>
+                            <a
+                                href="{{ route('admin.pages.index', ['filter' => true, 'status' => 'scheduled']) }}">Agendadas</a>
+                            <span class="mx-1">|</span>
+                            <a
+                                href="{{ route('admin.pages.index', ['filter' => true, 'status' => 'draft']) }}">Rascunho</a>
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-6 col-xl-4 mb-4">
+            <div class="card card-body d-flex flex-row align-items-center cards-list-item">
                 {{ icon_elem('appIndicator') }}
                 <div class="card-item-content">
                     <h5 class="h3 mb-0">Example</h5>
