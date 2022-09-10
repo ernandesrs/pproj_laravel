7ef73134c3391d723f86f04babeb9d790b4566a0
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-10 17:25:23 -0400

    Concluindo seções de exemplo no front

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -16,18 +16,52 @@
 <body>
     <header class="bg-dark sticky-top py-1">
         <div class="container py-2 d-flex align-items-center">
-            <a href="">{{ config('app.name') }}</a>
+            <a href="{{ route('front.index') }}" title="Página inicial">{{ config('app.name') }}</a>
             <nav class="nav ml-auto">
-                @auth
-                    @if (auth()->user()->level === 9)
-                        <a class="nav-link" href="{{ route('admin.index') }}">Administração</a>
-                    @endif
-                    <a class="nav-link" href="{{ route('member.index') }}">Painel</a>
-                    <a class="nav-link jsLogout" href="{{ route('auth.logout') }}">Sair</a>
-                @else
-                    <a class="nav-link" href="{{ route('auth.register') }}">Registro</a>
-                    <a class="nav-link" href="{{ route('auth.login') }}">Login</a>
-                @endauth
+                @php
+                    $user = auth()->user();
+                @endphp
+                <a class="btn btn-primary dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    {{ $user ? substr($user->name, 0, 8) . (strlen($user->name) > 8 ? '...' : null) : 'Conta' }}
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="px-3 py-1">
+                        @if ($user)
+                            <div class="d-flex flex-column justify-content-center align-items-center">
+                                <img class="img-fluid rounded-circle img-thumbnail"
+                                    src="{{ m_user_photo_thumb($user, 'sm') }}" alt="{{ $user->name }}"
+                                    style="width: 75px;">
+                                <div class="text-center font-weight-bold py-2 text-muted h5 mb-0">
+                                    {{ $user->name }}
+                                </div>
+                            </div>
+                            <div class="py-2">
+                                <a class="btn btn-sm btn-primary px-4" href="{{ route('member.profile') }}">
+                                    Perfil
+                                </a>
+                                <a class="btn btn-sm btn-outline-danger px-4" href="{{ route('auth.logout') }}">
+                                    Logout
+                                </a>
+                            </div>
+                            <div class="dropdown-divider"></div>
+                            <div class="py-2">
+                                <a class="dropdown-item" href="{{ route('admin.index') }}">
+                                    Admin
+                                </a>
+                                <a class="dropdown-item" href="{{ route('member.index') }}">
+                                    Membro
+                                </a>
+                            </div>
+                        @else
+                            <a class="btn btn-sm btn-primary px-4" href="{{ route('auth.login') }}">
+                                Login
+                            </a>
+                            <a class="btn btn-sm btn-outline-primary px-4" href="{{ route('auth.register') }}">
+                                Criar conta
+                            </a>
+                        @endif
+                    </div>
             </nav>
         </div>
     </header>
@@ -39,7 +73,6 @@
 
         @yield('content')
     </main>
-
 
     <footer class="bg-light-light border-top">
         <div class="container pt-5 pb-2">
@@ -68,7 +101,7 @@
                 </div>
 
                 <div class="col-12 col-sm-6 col-lg-4 d-flex flex-column justify-content-center align-items-center">
-                    <h3 class="h5 mb-0 font-weight-bold">Menu #1</h3>
+                    <h3 class="h5 mb-0 font-weight-bold">Menu #2</h3>
                     <nav class="nav flex-column">
                         <a class="nav-link" href="#">Link #1</a>
                         <a class="nav-link" href="#">Link #2</a>
@@ -82,7 +115,18 @@
         <div class="container py-2 pb-5">
             <div class="text-center">
                 <small>
-                    <a href="{{ route('front.index') }}">{{ config('app.name') }}</a> &copy; {{ date('Y') }}
+                    <p class="mb-0">
+                        <a href="{{ route('front.index') }}">{{ config('app.name') }}</a> &copy; {{ date('Y') }}
+                    </p>
+                    <p class="mb-0">
+                        <a href="{{ route('front.dinamicPage', ['slug' => 'politicas-de-privacidade']) }}">
+                            Políticas de privacidade
+                        </a>
+                        <span class="px-1">|</span>
+                        <a href="{{ route('front.dinamicPage', ['slug' => 'termos-de-uso']) }}">
+                            Termos de uso
+                        </a>
+                    </p>
                 </small>
             </div>
         </div>
