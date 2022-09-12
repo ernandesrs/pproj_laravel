3a94b0195449f6e005a14df024908ed759a45d92
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-12 11:01:30 -0400

    Navegação

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,10 +1,17 @@
-@if ($model ?? null)
+@php
+$navigation = $model
+    ->onEachSide(2)
+    ->links()
+    ->render();
+@endphp
+
+@if (!empty($navigation))
     <div class="d-flex justify-content-center justify-content-lg-end align-items-center pt-2 pb-4">
         @if ($text ?? null)
             <div class="d-none d-lg-block mr-2 font-weight-bold text-muted">
                 {{ $text }}:
             </div>
         @endif
-        {{ $model->onEachSide(2)->links() }}
+        {!! $navigation !!}
     </div>
 @endif
